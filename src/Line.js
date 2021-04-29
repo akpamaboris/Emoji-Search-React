74145644629824bf1998591ebea19daa3383d14e
Zeliwipin <zeliwipin@gmail.com>
2021-04-29 19:04:31 +0200

    added some style and the feature to copy on click

--- src/Line.js
+++ src/Line.js
@@ -5,8 +5,13 @@ const Line = ({ search, setSearch }) => {
     return emoji.slice(0, 20).map((x, index) => {
       return (
         <div>
-          <p key={index} className="emoji">
-            {x.symbol} {x.title}
+          <p
+            key={index}
+            className="emoji"
+            onClick={() => navigator.clipboard.writeText(x.symbol)}
+          >
+            {x.symbol} {x.title}{" "}
+            <span className="emoji-text"> Click to copy!</span>
           </p>
         </div>
       );
@@ -16,8 +21,13 @@ const Line = ({ search, setSearch }) => {
   const searchFunc = (x, index) => {
     if (x.keywords.toLowerCase().includes(search.toLowerCase())) {
       return (
-        <p key={index} className="emoji">
-          {x.symbol} {x.title}
+        <p
+          key={index}
+          className="emoji"
+          onClick={() => navigator.clipboard.writeText(x.symbol)}
+        >
+          {x.symbol} {x.title}{" "}
+          <span className="emoji-text"> Click to copy!</span>
         </p>
       );
     }
